ce1b02876b11125de3a247692884a1c931e32c67
locnxth1907005 <locnxth1907005@fpt.edu.vn>
2021-08-27 22:10:58 +0700

    1

---
+++ app/src/main/java/com/example/slide21/MainActivityPokemon.java
@@ -0,0 +1,14 @@
+package com.example.slide21;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivityPokemon  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainpokemon);
+    }
+
+}
